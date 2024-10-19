4bd2cfbf708137ac3296c9a2bc83280fb0e1abf6
Niteesh221FA19030 <niteeshnani334@gmail.com>
2024-10-20 04:11:55 +0530

    Updates

---
+++ precache-manifest.fbf4f2b44fde2274a40d812b867182af.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d132985a0fce8607db5304f72492878f",
+    "url": "/chat-website-application/index.html"
+  },
+  {
+    "revision": "c0e88b02cb7e3f0a6186",
+    "url": "/chat-website-application/static/css/2.a486e1fe.chunk.css"
+  },
+  {
+    "revision": "2a2927d3b3109d5445a4",
+    "url": "/chat-website-application/static/css/main.bf643cf5.chunk.css"
+  },
+  {
+    "revision": "c0e88b02cb7e3f0a6186",
+    "url": "/chat-website-application/static/js/2.a0275403.chunk.js"
+  },
+  {
+    "revision": "2a2927d3b3109d5445a4",
+    "url": "/chat-website-application/static/js/main.e4ead89f.chunk.js"
+  },
+  {
+    "revision": "072b8389bbf97fd7622b",
+    "url": "/chat-website-application/static/js/runtime~main.201e0b3c.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/fb.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/goo.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/ins.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/key.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/link.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/mail.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/chat-website-application/static/media/profile.d41d8cd9.svg"
+  }
+]);
